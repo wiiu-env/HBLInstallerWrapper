96cb77da405bc923c734fa45a3de87d335dd0aed
Maschell <Maschell@gmx.de>
2022-01-08 11:32:44 +0100

    Update hbl_install.cpp

--- source/hbl_install.cpp
+++ source/hbl_install.cpp
@@ -144,17 +144,16 @@ const unsigned char homebrew_stub[] =
 static void InstallPatches() {
     OsSpecifics osSpecificFunctions;
     memset(&osSpecificFunctions, 0, sizeof(OsSpecifics));
-
-    /* Pre-setup a few options to defined values */
-    OS_FIRMWARE = 550;
-    MAIN_ENTRY_ADDR = 0x00802000;
-    ELF_DATA_ADDR = 0xDEADC0DE;
-    ELF_DATA_SIZE = 0;
-    HBL_CHANNEL = 0;
-
+    
     // If we install the sd_loader but don't have any homebrew loaded some applications won't start.
     // We load a stub that just opens the real app and opens the hbl when opening the mii maker.
     memcpy((void *) 0x00802000, homebrew_stub, sizeof(homebrew_stub));
+
+    /* Pre-setup a few options to defined values */
+    OS_FIRMWARE = 550;
+    ELF_DATA_ADDR = 0xDEADC0DE;
+    ELF_DATA_SIZE = 0;
+    HBL_CHANNEL = 0;
 
     osSpecificFunctions.addr_OSDynLoad_Acquire = (unsigned int) OSDynLoad_Acquire;
     osSpecificFunctions.addr_OSDynLoad_FindExport = (unsigned int) OSDynLoad_FindExport;
